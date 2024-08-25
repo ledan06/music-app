ef92e7818b2031e20c72cf83da210041e25d7f78
ledan06 <webbackend123@gmail.com>
2024-08-25 15:32:23 +0700

    admin view

---
+++ controller/admin/song.controller.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+import Song from "../../models/song.model";
+import Topic from "../../models/topic.model";
+import Singer from "../../models/singer.model";
+import { systemConfig } from "../../config/config";
+
+//[GET] /admin/songs
+export const index = async (req: Request, res: Response) =>{
+    const songs = await Song.find({
+        deleted: false
+    })
+    res.render("admin/pages/song/index", {
+        pageTitle: "Quản lý bài hát",
+        songs: songs
+    })
+}
+
+//[GET] /admin/songs/create
+export const create = async (req: Request, res: Response) =>{
+    const singers = await Singer.find({
+        deleted: false
+    })
+    const topics = await Topic.find({
+        deleted: false
+    })
+    res.render("admin/pages/song/create", {
+        pageTitle: "Tạo bài hát",
+        singers: singers,
+        topics: topics
+    })
+}
+//[POST] /admin/songs/create
+export const createPost = async (req: Request, res: Response) =>{
+    let avatar = ""
+    let audio = ""
+
+    if(req.body.avatar){
+        avatar = req.body.avatar[0]
+    }
+
+    if(req.body.audio){
+        audio = req.body.audio[0]
+    }
+    const dataSong = {
+        title: req.body.title,
+        topicId: req.body.topicId,
+        singerId: req.body.singerId,
+        description: req.body.description,
+        status: req.body.status,
+        avatar: avatar,
+        audio: audio
+       }
+       const song = new Song(dataSong)
+       await song.save()
+       res.redirect(`/${systemConfig.prefixAdmin}/songs`)
+}
---
+++ routes/admin/songs.route.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+const router: Router = Router()
+import multer from "multer"
+
+import * as controller from "../../controller/admin/song.controller"
+import * as uploadCloud from "../../middlewares/admin/uploadCloud.middlewares"
+const upload = multer()
+
+router.get("/", controller.index)
+router.get("/create", controller.create)
+router.post(
+    "/create", 
+    upload.fields([
+        { name: 'avatar', maxCount: 1 }, 
+        { name: 'audio', maxCount: 1 }
+    ]), 
+    uploadCloud.uploadFields,
+    controller.createPost
+)
+
+export const songRoutes: Router = router
